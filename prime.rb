66ab84689886e184b9e61632f5d5067cd32aaeb2
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-17 22:46:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -14,7 +14,7 @@ def sieve(max)
     next unless p
 
     # Break if we are past the square root of the max value 
-    break if p*p => max
+    break if p*p > max
     counter += 1
     # Start at the square of the current number, and step through.
     # Go up to the max value, by multiples of the current number, and replace
