6de7d453bcdc22fab89261a2f126a79657c916b5
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-17 23:44:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,19 +6,16 @@ def sieve(max)
   # Iterate through primes array
   counter = 0
   primes.each do |p|
-    # Skip if nil
+
     next unless p
 
-    # Break if we are past the square root of the max value 
     break if p*p > max
     counter += 1
-    # Start at the square of the current number, and step through.
-    # Go up to the max value, by multiples of the current number, and replace
-    # that value with nil in the primes array
+  
     (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
-  # Finally, return the compacted array.
+ 
   puts "Solved for #{max} in #{counter} steps."
   primes.compact
 end
@@ -27,4 +24,3 @@ def prime?(num)
   sieve(num).include?(num)
 end
 
-puts prime?(105557)
